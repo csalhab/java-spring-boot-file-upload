0b071c6fb6e301b52005aa954745f6be30d779b3
Claudia Correa Salhab <claudiacorreasalhab@gmail.com>
2022-04-18 18:07:38 -0400

    added initial comments on annotations

--- initial/src/main/java/com/example/uploadingfiles/UploadingFilesApplication.java
+++ initial/src/main/java/com/example/uploadingfiles/UploadingFilesApplication.java
@@ -9,6 +9,16 @@ import org.springframework.context.annotation.Bean;
 import com.example.uploadingfiles.storage.StorageProperties;
 import com.example.uploadingfiles.storage.StorageService;
 
+/*
+@SpringBootApplication is a convenience annotation that adds all of the following:
+
+@Configuration: Tags the class as a source of bean definitions for the application context.
+
+@EnableAutoConfiguration: Tells Spring Boot to start adding beans based on classpath settings, other beans, and various property settings. For example, if spring-webmvc is on the classpath, this annotation flags the application as a web application and activates key behaviors, such as setting up a DispatcherServlet.
+
+@ComponentScan: Tells Spring to look for other components, configurations, and services in the com/example package, letting it find the controllers.
+*/
+
 @SpringBootApplication
 @EnableConfigurationProperties(StorageProperties.class)
 public class UploadingFilesApplication {
